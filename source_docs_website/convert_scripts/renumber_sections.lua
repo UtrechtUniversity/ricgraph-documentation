0f6350a343c2566ca230e2a401a3f848b918a23c
Rik D.T. Janssen <121875841+RikDTJanssen@users.noreply.github.com>
2025-04-15 14:00:15 +0200

    Sections on the documentation webpages now have a different numbering scheme: # gets no number and ## gets a single number

---
+++ source_docs_website/convert_scripts/renumber_sections.lua
@@ -0,0 +1,50 @@
+-- This script renumbers section headings in GitHub Markdown files.
+-- It will:
+-- Markdown # -> Unnumber H1 headings (no prefix)
+-- Markdown ## ->: Number H2 as 1, 2, etc. (independent counter)
+-- Markdown ### ->: Number H3 as 1.1, 1.2, etc. (resetting per H2 section)
+-- Markdown #### ->: Number H4 as 1.1.1, 1.1.2, etc. (resetting per H3 subsec)
+
+local h2_count = 0
+local h3_count = 0
+local h4_count = 0
+
+function Header(el)
+  if el.level == 1 then
+    -- Reset all counters for new top-level section
+    h2_count = 0
+    h3_count = 0
+    h4_count = 0
+    el.classes:insert('unnumbered')
+    return el
+
+  elseif el.level == 2 then
+    -- Increment H2 counter and reset H3/H4
+    h2_count = h2_count + 1
+    h3_count = 0
+    h4_count = 0
+    local num = pandoc.Str(tostring(h2_count) .. " ")
+    table.insert(el.content, 1, num)
+    el.classes:insert('unnumbered')
+    return el
+
+  elseif el.level == 3 then
+    -- Increment H3 counter and reset H4
+    h3_count = h3_count + 1
+    h4_count = 0
+    local num = pandoc.Str(tostring(h2_count) .. "." .. tostring(h3_count) .. " ")
+    table.insert(el.content, 1, num)
+    el.classes:insert('unnumbered')
+    return el
+
+  elseif el.level == 4 then
+    -- Increment H4 counter
+    h4_count = h4_count + 1
+    local num = pandoc.Str(tostring(h2_count) .. "." .. tostring(h3_count) .. "." .. tostring(h4_count) .. " ")
+    table.insert(el.content, 1, num)
+    el.classes:insert('unnumbered')
+    return el
+  end
+end
+
+
