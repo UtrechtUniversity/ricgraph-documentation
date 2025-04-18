6558092699ee0fb176cfbe654101496b1b25ceba
Rik D.T. Janssen <121875841+RikDTJanssen@users.noreply.github.com>
2025-04-18 17:28:07 +0200

    Added comment

--- source_docs_website/convert_scripts/set_webpage_title.lua
+++ source_docs_website/convert_scripts/set_webpage_title.lua
@@ -1,6 +1,10 @@
 -- This script sets the webpage title to the <h1> heading,
 -- that is the '#' in the md file. It ensures that <title> has
 -- a useful value, i.e. "Read more..." instead of "read-more".
+-- -------------------------------------------------------------------------
+-- Rik D.T. Janssen, original version April, 2025.
+-- -------------------------------------------------------------------------
+
 function Pandoc(doc)
   -- Find the first heading in the document
   local first_h1 = nil
